f715983c0d20f4378f7f18144a02956cbd7c6188
fede <federicoresano1@gmail.com>
2024-02-25 16:24:12 -0300

    Migrated to lua

---
+++ lua/myconfig/lazyvim.lua
@@ -0,0 +1,36 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+    {"savq/melange-nvim", lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme melange]])
+    end,
+    },
+	"nvim-telescope/telescope.nvim",
+	"nvim-treesitter/nvim-treesitter",
+	"tpope/vim-fugitive",
+	"VonHeikemen/lsp-zero.nvim",
+	"neovim/nvim-lspconfig",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-nvim-lua",
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"saadparwaiz1/cmp_luasnip",
+	"L3MON4D3/LuaSnip",
+	"rafamadriz/friendly-snippets",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+
+}, opts)
