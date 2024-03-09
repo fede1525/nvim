01115620006dbc27abee886bc7ee139fd607b0ae
fede <federicoresano1@gmail.com>
2024-03-09 00:15:49 -0300

    Added remaps for tabs

--- lua/myconfig/remap.lua
+++ lua/myconfig/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = ";"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "tn", vim.cmd.tabnew)
+vim.keymap.set("n", "<PageUp>", vim.cmd.tabn)
+vim.keymap.set("n", "<PageDown>", vim.cmd.tabp)
