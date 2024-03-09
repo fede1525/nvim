38cc26872903fff2d56b137a85d04f9706345698
fede <federicoresano1@gmail.com>
2024-03-09 17:25:52 -0300

    Added keymaps

--- lua/myconfig/remap.lua
+++ lua/myconfig/remap.lua
@@ -1,5 +1,7 @@
-vim.g.mapleader = ";"
+﻿vim.g.mapleader = ";"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "tn", vim.cmd.tabnew)
+vim.keymap.set("n", "tc", vim.cmd.tabclose)
 vim.keymap.set("n", "<PageUp>", vim.cmd.tabn)
 vim.keymap.set("n", "<PageDown>", vim.cmd.tabp)
+vim.keymap.set("t", '<esc>', [[<C-\><C-n>]])
